75451b0728961a2fac04ca22d34882c6c5942721
AyakoGFX <ayako.gfx@gmail.com>
2024-06-18 22:53:37 +0530

    colours updated

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 #include "layouts.c"
 #include "movestack.c"
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -16,14 +16,14 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 
 /*static const char col_cyan[]        = "#005577";*/
-
-static const char col_red[]        = "#cc241d";
+/*active window*/
+static const char col_border[]        = "#eeeeee";
 
 static const char col_cyan[]        = "#222222";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_red  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
 
 	/*[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },*/
 };
