5da41e964a46f0c897ef50af57aec6c67f8e6b5f
AyakoGFX <ayako.gfx@gmail.com>
2024-06-18 16:51:55 +0530

    keybindings for the brightness controls

--- config.h
+++ config.h
@@ -62,6 +62,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 /*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };*/
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *brightness_up[] = { "brightnessctl", "set", "5%+", NULL };
+static const char *brightness_down[] = { "brightnessctl", "set", "5%-", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -83,6 +85,10 @@ static const Key keys[] = {
 
 	{ MODKEY|ControlMask,	        	XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
+
+/*brightness*/
+  { MODKEY,                       XK_w,      spawn,          {.v = brightness_up } },
+  { MODKEY,                       XK_s,      spawn,          {.v = brightness_down } },
 
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
