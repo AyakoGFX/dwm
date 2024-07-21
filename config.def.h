633db065fc0ba5c9320cc588fdb4935b2bf4dad3
ayako <ayako.gfx@gmail.com>
2024-07-22 03:46:42 +0530

    z

--- config.def.h
+++ config.def.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
+#include "theme.c"
 #include "layouts.c"
 #include "movestack.c"
 /* appearance */
@@ -10,22 +11,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
 
-/*static const char col_cyan[]        = "#005577";*/
 /*active window*/
-static const char col_border[]        = "#eeeeee";
 
-static const char col_cyan[]        = "#222222";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
+	// [SchemeSel]  = { col_gray4, col_cyan,  col_AW },
+  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 
-	/*[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },*/
 };
 
 /* tagging */
---
+++ theme.c
@@ -0,0 +1,40 @@
+
+// darkblue
+static const char col_gray1[]       = "#16161e";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#16161e";
+static const char col_gray5[]       = "#ca6278";
+static const char col_cyan[]        = "#68aad2";
+
+
+// Dracula
+// static const char col_gray1[]       = "#282a36";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#f8f8f2";
+// static const char col_gray4[]       = "#282a36";
+// static const char col_gray5[]       = "#a272a4";
+// static const char col_cyan[]        = "#bf93f9";
+
+
+// gruv
+// static const char col_gray1[]       = "#282828";
+// static const char col_gray2[]       = "#222222";
+// static const char col_gray3[]       = "#ebdbb2";
+// static const char col_gray4[]       = "#282828";
+// static const char col_gray5[]       = "#8ec07c";
+// static const char col_cyan[]        = "#d65d0e";
+
+// static const char col_gray1[]       = "#1e1e2e";
+// static const char col_gray2[]       = "#181825";
+// static const char col_gray3[]       = "#cdd6f4";
+// static const char col_gray4[]       = "#181825";
+// static const char col_gray5[]       = "#b4befe";
+// static const char col_cyan[]        = "#f5c2e7";
+//
+// static const char col_gray1[]       = "#222222";
+// static const char col_gray2[]       = "#444444";
+// static const char col_gray3[]       = "#bbbbbb";
+// static const char col_gray4[]       = "#eeeeee";
+// static const char col_cyan[]        = "#222222";
+// static const char col_AW[]        = "#eeeeee";
