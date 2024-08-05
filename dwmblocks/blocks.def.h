c8cc92885b4ee6a426a36b6a2a98279409afbc03
ayako <ayako.gfx@gmail.com>
2024-08-05 16:32:33 +0530

    some how fixed the broken error

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -6,11 +6,10 @@ static const Block blocks[] = {
   {"", "date '+%d/%m/%y'", 5, 0},
   {"", "date '+%I:%M %p'", 5, 0}, // 12-hour format with AM/PM
   {"Up:", "uptime | grep -ohe 'up .*' | awk '{gsub(/[,;]/, \"\", $2); print $2}'", 60, 0}, // Uptime block
-  //
-  {"R:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  {"R:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	3,		0},
   {"B:", "acpi | awk '{ print $5 }' | tr -d ','", 60, 0},
-
   {"D:",   "df -h / | awk 'NR==2 {print $3, $4}'", 60, 0}, // Used space on /
+  {"V:", "pactl list sinks | awk '/Volume:/ {print $5}' | head -n 1 | tr -d '%')%",1 ,0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
