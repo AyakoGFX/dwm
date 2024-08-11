3d94cedec3adbbd62b7227d17794ccdae7da8723
AyakoGFX <ayako.gfx@gmail.com>
2024-08-11 14:32:49 +0530

    d

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 sudo pacman -S --needed --noconfirm base-devel extra/git extra/libx11 extra/libxcb extra/libxinerama extra/libxft extra/imlib2
 sudo pacman -S --noconfirm picom flameshot rofi dmenu acpi nitrogen polkit-gnome google-chrome pcmanfm xfce4-taskmanager btop brightnessctl
 
+
+# sudo apt install build-essential git libx11-dev libx11-xcb-dev libxcb-res0-dev libxinerama-dev libxft-dev libimlib2-dev
+# below pkg not tested
+#sudo pacman -S --noconfirm picom flameshot rofi dmenu acpi nitrogen polkit-gnome google-chrome pcmanfm xfce4-taskmanager btop brightnessctl
+
+
 cp -r .dwm ~/
 
 make
@@ -17,6 +23,4 @@ cd ..
 
 sudo make clean install 
 #&& rm *.o && rm *.orig
-
-
 
