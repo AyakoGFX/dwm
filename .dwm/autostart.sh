92f86f00a9b11fb188e99edffd6eaed4bbb278ed
ayako <ayako.gfx@gmail.com>
2024-11-11 21:15:28 +0530

    z

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,48 +6,40 @@ function run {
     fi
 }
 run sxhkd &
-
-# Restore wallpaper using Nitrogen
 run nitrogen --restore &
-
-# Start Picom for compositing
 run picom &
-
-# yay -S picom-ftlabs-git
-# picom --animations -b &
-
 run emacs --daemon &
+run nm-applet &
+run copyq &
+run slstatus &
+
+# Set keyboard repeat rate 
+run xset r rate 250 60 &
+setxkbmap -option caps:swapescape &
+
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
+
+
 
 #clipmenud &
-
-# Set keyboard repeat rate
-run xset r rate 250 60 &
+#blueman-applet &
+#volapplet &
+#cbatticon &
 
 # set screan to  1920x1080 --rate 144.00
 # xrandr --output eDP --mode 1920x1080 --rate 144.00 &
 # xrandr --output eDP --mode 1920x1080 --rate 60.02 &
 
 # dwmblocks &
-
-run nm-applet &
-
-run copyq &
-
-#blueman-applet &
-
-#volapplet &
-
-#cbatticon &
-
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/x86_64-linux-gnu/xfce4/notifyd/xfce4-notifyd &
-setxkbmap -option caps:swapescape &
 
 # mouse speed
 # xinput --set-prop '19' 'libinput Accel Profile Enabled' 0 1 0
 # xinput --set-prop '19' 'libinput Accel Speed' -0.1
 
+# yay -S picom-ftlabs-git
+# picom --animations -b &
 
 # setwall 1 &
 # for more info >> https://github.com/zeroruka/video-wallpaper-scripts.git
